7d7f6daccd13ef703904751e87e0acb654ef6460
hosterho <hauke.osterholz@gmail.com>
2018-01-02 19:29:43 +0100

    random, top-down and bottom-up implementations

--- automatic-random-grafter.py
+++ automatic-random-grafter.py
@@ -6,18 +6,16 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 counter = 0
-while counter is not 2147483647:
-
+while True is True:
     seed = os.popen('/usr/local/microsoft/powershell/6.0.0-rc.2/pwsh '
-                    '/Users/haos/PycharmProjects/SeedGrafter/seeds/seed_generator/specific_seed.ps1 ' + str(
-                     counter)).read()[:-1]
+                    '/Users/haos/PycharmProjects/SeedGrafter/seeds/seed_generator/random_seed.ps1').read()[:-1]
     log_string = 'Seed No.: ' + str(counter) + '  -  Seed: ' + str(seed)
     counter = counter + 1
     api = Iota('http://5.9.149.169:14265', seed)
     api_response = api.get_account_data(stop=1)
     if api_response['balance'] is not 0 or len(api_response['bundles']) is not 0:
         log_string = log_string + ' <-- is working'
-        textFile = open('/Users/haos/PycharmProjects/SeedGrafter/seeds/working_seeds/bottom_up', 'a')
+        textFile = open('/Users/haos/PycharmProjects/SeedGrafter/seeds/working_seeds/random_seeds', 'a')
         textFile.write('Seed: ' + str(seed) + '\n')
         textFile.write('Balance: ' + str(api_response['balance']) + '\n')
         textFile.write('Addresses: ' + str(api_response['addresses']) + '\n')
---
+++ automatic-top-down-grafter.py
@@ -0,0 +1,27 @@
+from iota import Iota
+import sys
+import os
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+counter = 2147471461  # 2147483647
+while counter is not 0:
+    seed = os.popen('/usr/local/microsoft/powershell/6.0.0-rc.2/pwsh '
+                    '/Users/haos/PycharmProjects/SeedGrafter/seeds/seed_generator/specific_seed.ps1 ' + str(
+                     counter)).read()[:-1]
+    log_string = 'Seed No.: ' + str(counter) + '  -  Seed: ' + str(seed)
+    api = Iota('http://5.9.149.169:14265', seed)
+    api_response = api.get_account_data(stop=1)
+    if api_response['balance'] is not 0 or len(api_response['bundles']) is not 0:
+        log_string = log_string + ' <-- is working'
+        textFile = open('/Users/haos/PycharmProjects/SeedGrafter/seeds/working_seeds/top_down', 'a')
+        textFile.write('Seed: ' + str(seed) + '\n')
+        textFile.write('Balance: ' + str(api_response['balance']) + '\n')
+        textFile.write('Addresses: ' + str(api_response['addresses']) + '\n')
+        textFile.write('Bundles: ' + str(api_response['bundles']) + '\n')
+        textFile.write('\n')
+        textFile.write('\n')
+        textFile.close()
+    counter = counter - 1
+    print(log_string)
